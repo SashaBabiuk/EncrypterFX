3367feea5e0f463b59894a721b8a5046a41dad1b
oleksandr babiuk <sashasbabuk82@gmail.com>
2025-02-28 15:12:20 +0200

    Change unit test for JUnit 5

--- src/test/java/org/encrypter/Model/Encryptor/BaseEncryptorTest.java
+++ src/test/java/org/encrypter/Model/Encryptor/BaseEncryptorTest.java
@@ -1,34 +1,33 @@
 package org.encrypter.Model.Encryptor;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 
-@RunWith(Parameterized.class)
-public class BaseEncryptorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BaseEncryptorTest {
 
     private static final String ARABIC_ALPHABET = "ى ء آ ؤ إ ئ ة\n؛ ، ؟ا ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن ه و ي ى ء آ ؤ إ ئ ة ؛ ، ؟";
     private static final String CHINESE_ALPHABET = "的一是不了人我在有他这为之大来以个中上们到说国和地也子时道出而要于就下得可你年生会着对后自里那学好看起发当么事";
     private static final String UKRAINIAN_ALPHABET = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгґдеєжзиіїйклмнопрстуфхцчшщьюя.,«»\"':!? ";
     private static final String ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,.!? ";
 
-    private final String inputText;
-    private final String expectedEncodedText;
-    private final String alphabet;
-
-    // Конструктор параметризованого тесту
-    public BaseEncryptorTest(String inputText, String expectedEncodedText, String alphabet) {
-        this.inputText = inputText;
-        this.expectedEncodedText = expectedEncodedText;
-        this.alphabet = alphabet;
+    static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of("Hello, World!", "Khoor?CZruogA", ENGLISH_ALPHABET),
+                Arguments.of("Привіт, світ!", "Туйдкх\"ВфдкхА", UKRAINIAN_ALPHABET),
+                Arguments.of("你好，世界！", "会发，世界！", CHINESE_ALPHABET)
+        );
     }
 
-    @Test
-    public void testEncodeAndDecodeContent() {
+    @ParameterizedTest(name = "Test encoding and decoding functionality")
+    @MethodSource("provideTestCases")
+    void testEncodeAndDecodeContent(String inputText, String expectedEncodedText, String alphabet) {
+        System.out.println("Testing with alphabet: " + alphabet);
         int key = 3;
 
         // Шифруємо
@@ -38,20 +37,5 @@ public class BaseEncryptorTest {
         // Дешифруємо
         String decoded = new BaseEncryptor(alphabet).decodeContent(encoded, key);
         assertEquals(inputText, decoded);
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                // Англійська мова
-                {"Hello, World!", "Khoor?CZruogA", ENGLISH_ALPHABET},
-
-                // Українська мова
-                {"Привіт, світ!", "Туйдкх\"ВфдкхА", UKRAINIAN_ALPHABET},
-
-                // Китайська мова
-                {"你好，世界！", "会发，世界！", CHINESE_ALPHABET},
-
-        });
     }
 }
